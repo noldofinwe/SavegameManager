42b466cd295dd46a014a121895c5403fd4651930
michel <mobbink@gmail.com>
2026-03-28 21:30:28 +0100

    blup

---
+++ ATGSaveGameManager.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ATGSaveGameManager.Avalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ATGSaveGameManager.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ATGSaveGameManager.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
