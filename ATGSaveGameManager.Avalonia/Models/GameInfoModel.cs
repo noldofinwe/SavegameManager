0d1b7ba56c78f4e033495f62bf480a14b0fc0c53
michel <mobbink@gmail.com>
2026-03-28 20:39:57 +0100

    Start

---
+++ ATGSaveGameManager.Avalonia/Models/GameInfoModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace ATGSaveGameManager
+{
+    public class GameInfoModel : ViewModelBase
+    {
+        public string Name { get; set; }
+
+        public string FileName { get; set; }
+
+        public string[] Players { get; set; }
+        public string LastPlayer { get; set; }
+
+        public int? CurrentTurn { get; set; }
+        public DateTime LastTurnTime { get; set; }
+
+
+        public string GameType { get; set; }
+
+    }
+}
---
+++ ATGSaveGameManager.Avalonia/Models/GameType.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ATGSaveGameManager
+{
+    public class GameType
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("extension")]
+        public string Extension { get; set; }
+
+        [JsonProperty("savegames")]
+        public string Savegames { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+
+
+    }
+}
