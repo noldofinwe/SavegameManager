9061ab7195bb76a7d719a134f9f776aff20a3bd2
Michel Obbink <michel.obbink@rabobank.com>
2026-04-08 13:16:03 +0200

    Fix Windows?

--- ATGSaveGameManager.Avalonia/Configuration/WindowsSecureStorage.cs
+++ ATGSaveGameManager.Avalonia/Configuration/WindowsSecureStorage.cs
@@ -1,34 +1,47 @@
+﻿using ATGSaveGameManager.Configuration;
 using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using System.Security.Cryptography;
-namespace ATGSaveGameManager.Configuration;
-
-
-#if WINDOWS
 
 public class WindowsSecureStorage : ISecureStorage
 {
     public Task StoreAsync(string key, string value)
     {
         var bytes = Encoding.UTF8.GetBytes(value);
-        var protectedBytes = ProtectedData.Protect(bytes, null, DataProtectionScope.CurrentUser);
+        var protectedBytes = ProtectedData.Protect(
+            bytes,
+            null,
+            DataProtectionScope.CurrentUser);
+
         File.WriteAllBytes(GetPath(key), protectedBytes);
         return Task.CompletedTask;
     }
 
     public Task<string?> RetrieveAsync(string key)
     {
-        if (!File.Exists(GetPath(key)))
+        var path = GetPath(key);
+
+        if (!File.Exists(path))
             return Task.FromResult<string?>(null);
 
-        var protectedBytes = File.ReadAllBytes(GetPath(key));
-        var bytes = ProtectedData.Unprotect(protectedBytes, null, DataProtectionScope.CurrentUser);
+        var protectedBytes = File.ReadAllBytes(path);
+        var bytes = ProtectedData.Unprotect(
+            protectedBytes,
+            null,
+            DataProtectionScope.CurrentUser);
+
         return Task.FromResult<string?>(Encoding.UTF8.GetString(bytes));
     }
 
-    private string GetPath(string key) =>
-        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MyApp", key);
+    private string GetPath(string key)
+    {
+        var directory = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "ATGSavegameManager");
+
+        Directory.CreateDirectory(directory);
+
+        return Path.Combine(directory, key);
+    }
 }
-#endif
