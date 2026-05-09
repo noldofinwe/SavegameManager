c3ad2266d9abf0e66e7ce6195cc39505375650c9
michel <mobbink@gmail.com>
2026-05-09 22:17:06 +0200

    Changes

--- ATGSaveGameManager.Avalonia/ViewModels/MainViewModel.cs
+++ ATGSaveGameManager.Avalonia/ViewModels/MainViewModel.cs
@@ -11,7 +11,6 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Linq;
